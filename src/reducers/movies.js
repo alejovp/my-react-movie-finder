02d99230d0f744c7757aac03cac7e8c8407ea839
Alejandro <alejovp@gmail.com>
2018-06-04 04:08:45 +0200

    Added search cases to movie reducer

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -1,7 +1,11 @@
 import { 
   FETCH_POP_MOVIES,
   FETCH_POP_MOVIES_OK,
-  FETCH_POP_MOVIES_KO } from '../actions';
+  FETCH_POP_MOVIES_KO,
+  SEARCH_MOVIE,
+  SEARCH_MOVIE_OK,
+  SEARCH_MOVIE_KO
+ } from '../actions';
 
 const defaultState = {
   isLoading: false,
@@ -12,13 +16,24 @@ export default function (state = defaultState, action) {
   switch (action.type) {
     case FETCH_POP_MOVIES:
       return { ...state, isLoading: true };
+
+    case SEARCH_MOVIE:
+      return { ...state, isLoading: true };
     
     case FETCH_POP_MOVIES_OK: 
-      const { results } = action.payload.data;
+      const popMovies = action.payload.data.results;
       
-      return { moviesData: results, isLoading: false };
+      return { moviesData: popMovies, isLoading: false };
+    
+    case SEARCH_MOVIE_OK: 
+      const movieResult = action.payload.data.results;
+      
+      return { moviesData: movieResult, isLoading: false };
 
     case FETCH_POP_MOVIES_KO:
+      return { ...defaultState };
+    
+    case SEARCH_MOVIE_KO:
       return { ...defaultState };
 
     default:
